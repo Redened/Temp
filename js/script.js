6d80e3eec790cd34fd57929878c54127330c9482
Redened <85760145+Redened@users.noreply.github.com>
2025-02-24 17:46:14 +0400

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("mNavOpen").addEventListener("click", function(e) {
+    document.getElementById("mNav").classList.remove("hidden")
+});
+
+document.getElementById("mNavClose").addEventListener("click", function(e) {
+    document.getElementById("mNav").classList.add("hidden")
+});
